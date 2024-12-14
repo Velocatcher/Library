8dc9887430a5b7b508a8df11ab6fa9fe2932a4a2
Vladimir <eurasian@list.ru>
2024-12-14 08:17:29 +0300

    Added admin.html

--- src/main/java/com/books/library/config/SecurityConfig.java
+++ src/main/java/com/books/library/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -21,31 +26,47 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // Обновлено использование метода csrf
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/index", "/register", "/css/**", "/js/**").permitAll() // Доступ без авторизации
+                        .requestMatchers("/", "/index", "/register", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/home").hasRole("USER")
                         .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated() // Все остальные запросы требуют авторизации
+                        .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
-                                .loginPage("/login")
-                                .defaultSuccessUrl("/home", true) // Перенаправление на /home после входа
-                                .successHandler(new CustomAuthenticationSuccessHandler())
-                                .permitAll()
-//                .formLogin((form) -> form
-//                        .loginPage("/login")
-//                        .successHandler(new CustomAuthenticationSuccessHandler()) // Указан кастомный обработчик
-//                        .permitAll()
+                        .loginPage("/login")
+                        .successHandler(customAuthenticationSuccessHandler()) // Указываем кастомный обработчик
+                        .permitAll()
                 )
                 .logout((logout) -> logout.permitAll());
 
         return http.build();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                org.springframework.security.core.Authentication authentication)
+                    throws IOException, ServletException {
+                authentication.getAuthorities().forEach(authority -> {
+                    try {
+                        if (authority.getAuthority().equals("ROLE_ADMIN")) {
+                            response.sendRedirect("/admin");
+                        } else if (authority.getAuthority().equals("ROLE_USER")) {
+                            response.sendRedirect("/home");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        };
+    }
 }
-
